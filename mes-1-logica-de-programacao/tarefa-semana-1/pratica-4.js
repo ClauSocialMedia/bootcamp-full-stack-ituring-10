47ce299d5d57a85f1980f83a48822cd91a119e45
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-12 12:24:32 -0300

    tabuada

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-4.js
@@ -0,0 +1,7 @@
+const MILHAPARAKM = 1.60934
+const distanciaTerraLuaEmMilhas = 238900
+const distanciaTerraLuaEmKm = 238900*MILHAPARAKM
+
+
+console.log(distanciaTerraLuaEmKm)
+
---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,12 @@
+function meusDados(nome, email, profissao, idade, lugarSonho){
+   
+ 
+}
+ nome ="Claudia"
+ email = "claudiapiurkoski@gmail.com"
+ profissao = "supervisora"
+ idade = 46
+ lugarSonho = "Noronha"
+
+mensagemwhatsApp =( `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarSonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+console.log(mensagemwhatsApp) 
