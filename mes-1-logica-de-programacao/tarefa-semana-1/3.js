741f3fb15360fab561d985438f4c5b46b84cbc4e
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-12 11:46:41 -0300

    Onde quero ir

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/3.js
@@ -0,0 +1,6 @@
+function debug(variavel){
+    const variavel = 12
+    const tipo = typeof variavel
+
+    console.log `[DEBUG] Valor: ${Variavel}`
+}
---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,5 @@
+function dados (nome, email, profissão, idade, lugarDosSonhos) {
+    console.log(nome, email, profissão, idade, lugarDosSonhos)
+}
+
+dados("Claudia", "claudiaoiuskoski@hotmail.com", "supervisora", 46 ,"Noronha")
