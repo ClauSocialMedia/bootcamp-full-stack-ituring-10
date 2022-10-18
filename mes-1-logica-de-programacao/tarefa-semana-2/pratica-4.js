dbf203cc6a8e91b72b1f66bb6a543d5c4db84e5c
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-17 22:12:02 -0300

    desconto

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
@@ -0,0 +1,7 @@
+function calcularDesconto(valorProduto, desconto) {
+    let valorDesconto = valorProduto * desconto/100;
+    let totalMenosDesconto = valorProduto - valorDesconto;
+    return totalMenosDesconto
+}
+
+console.log(calcularDesconto(100, 10))
