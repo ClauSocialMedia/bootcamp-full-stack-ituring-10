22f7aa68bd441181ca4a182623ee05fcd0ee95ff
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-31 09:35:44 -0300

    series

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -0,0 +1,20 @@
+function buscaTextoEmLista(lista, termoDeBusca) {
+    let listaFiltrada = []
+    for (let i = 0; i < lista.length; i ++, j++)  {
+
+        if (lista[i].includes(termoDeBusca)) {
+            
+            listaFiltrada[j] = lista[i]
+
+
+            listaFiltrada.push(listaSeriados)
+        }
+    }
+
+    return listaFiltrada
+}
+
+
+let listaSeriados = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+
+console.log(listaSeriados)
