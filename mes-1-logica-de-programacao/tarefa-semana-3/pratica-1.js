14e7fbcb2d129afa21e9b834e3cf65b22ac3fff6
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-31 09:27:54 -0300

    tabuada

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-1.js
@@ -0,0 +1,19 @@
+function tabuada(x, n) {
+    let i = 1;
+    let tabuada = 1
+    while (i <= n) {
+        tabuada = x*i
+        console.log (tabuada)
+        i += 1
+    }
+}
+tabuada(1,10)
+tabuada(2,10)
+tabuada(3,10)
+tabuada(4,10)
+tabuada(5,10)
+tabuada(6,10)
+tabuada(7,10)
+tabuada(8,10)
+tabuada(9,10)
+tabuada(10,10)
