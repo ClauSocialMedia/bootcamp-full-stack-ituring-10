6962dc3407f01f7fcfa1c40f9eb45543eb8ba203
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-10-31 21:41:17 -0300

    preferido

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -0,0 +1,17 @@
+let serie = []
+function favorito(lista, termoDeBusca) {
+    for (let i = 0; i < lista.length; i++) {
+
+        if (lista[i].includes(termoDeBusca)) {
+
+            listaFiltrada.push(serie)
+        }
+    }
+
+    return se
+
+}
+
+
+let preferido = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+console.log(preferido[4])
