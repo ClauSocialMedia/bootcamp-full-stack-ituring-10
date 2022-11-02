ccb1b42f213f29a230172c56a6345a2b7be43350
Claudia Lopes <claudiapiurkoski@gmail.com>
2022-11-02 16:18:21 -0300

    grupo

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-12.js
@@ -0,0 +1,12 @@
+function separarEmGrupos(){
+    let grupo = []
+    let grupo1 =["Anderson Luz", "Fabio Vechia" ];
+    let grupo2 = ["Claudia Lopes", "Rafael Alves"];
+    let grupo3 = ["Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"]
+
+    console.log(separarEmGrupos)
+
+   
+}
+minhaLista = separarEmGrupos
+let grupo =  (["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"])
